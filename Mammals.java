31e9fdfa7790c4b03b427d38609e576d12a190ac
Syanima <syanima@thoughtworks.com>
2016-03-17 14:13:47 +0530

    practiced interface in java

---
+++ Mammals.java
@@ -0,0 +1,23 @@
+class Mammals implements Animal{
+
+   public void eat(){
+      System.out.println("Mammal eats");
+   }
+
+   public void travel(){
+      System.out.println("Mammal travels");
+   } 
+
+   public int noOfLegs(){
+      return 0;
+   }
+
+   public static void main(String args[]){
+      Mammals m = new Mammals();
+      m.eat();
+      m.travel();
+   }
+   
+} 
+
+
